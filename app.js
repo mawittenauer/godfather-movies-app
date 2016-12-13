d51874512aca3ecf849da21f7c4acd4b5c07da02
Mike Wittenauer <mikewittenauerwebdevelopment@gmail.com>
2016-12-13 18:47:53 -0500

    Add routes for home, movie and 404 page

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req, res) {
-  res.send("Hello, World!");
-});
+var routes = require('./routes');
+
+app.get('/', routes.home);
+
+app.get('/part/:movie_number?', routes.movie_page);
+
+app.get('*', routes.not_found);
 
 app.listen(8080, function(){
   console.log("App listening on port 8080");
---
+++ routes/index.js
@@ -0,0 +1,13 @@
+exports.home = function(req, res) {
+  res.send('This is the home page');
+};
+
+exports.movie_page = function(req, res) {
+  var movie_number = req.params.movie_number;
+  
+  res.send('This is the page for The Godfather Part ' + movie_number);
+};
+
+exports.not_found = function(req, res) {
+  res.send('This page was not found');
+};
