7ea534684d547d98b1b5d031d621b3a2d7345eb1
Mike Wittenauer <mikewittenauerwebdevelopment@gmail.com>
2016-12-14 16:39:27 -0500

    Install ejs templating module

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname + '/public'));
+
 var routes = require('./routes');
 
 app.get('/', routes.home);
