5bcd5df981fc1679cc2aa69224c078c9328890f0
Mike Wittenauer <mikewittenauerwebdevelopment@gmail.com>
2016-12-17 19:53:48 -0500

    Prepare for heroku deployment

--- app.js
+++ app.js
@@ -15,6 +15,4 @@ app.get('/part/:movie_number?', routes.movie_page);
 
 app.get('*', routes.not_found);
 
-app.listen(8080, function(){
-  console.log("App listening on port 8080");
-});
+app.listen(process.env.PORT || 8080);
